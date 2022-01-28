f8d47d1ae68bd226aea0b1ac746211cc8216460b
Igor Vinicius <50421946+utrmliha@users.noreply.github.com>
2022-01-28 08:00:07 -0300

    Update ClienteSpecification.java

--- src/main/java/com/br/neoapp/specification/ClienteSpecification.java
+++ src/main/java/com/br/neoapp/specification/ClienteSpecification.java
@@ -8,27 +8,42 @@ import java.time.LocalDate;
 public class ClienteSpecification {
 
     public static Specification<Cliente> nome(String nome){
-        return (root, query, criteriaBuilder) ->
-            criteriaBuilder.like(root.get("nome"), "%"+nome+"%");
+        return (root, query, criteriaBuilder) -> {
+            if (nome == null)
+                return criteriaBuilder.conjunction();
+            return criteriaBuilder.like(root.get("nome"), "%"+nome+"%");
+        };
     }
 
     public static Specification<Cliente> celular(String celular){
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("celular"), celular);
+        return (root, query, criteriaBuilder) -> {
+            if (celular == null)
+                return criteriaBuilder.conjunction();
+            return criteriaBuilder.equal(root.get("celular"), celular);
+        };
     }
 
     public static Specification<Cliente> email(String email){
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("email"), email);
+        return (root, query, criteriaBuilder) -> {
+            if (email == null)
+                return criteriaBuilder.conjunction();
+            return criteriaBuilder.equal(root.get("email"), email);
+        };
     }
 
     public static Specification<Cliente> cpf(String cpf){
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("cpf"), cpf);
+        return (root, query, criteriaBuilder) -> {
+            if (cpf == null)
+                return criteriaBuilder.conjunction();
+            return criteriaBuilder.equal(root.get("cpf"), cpf);
+        };
     }
 
     public static Specification<Cliente> dataDeNascimento(LocalDate dataDeNascimento){
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("dataDeNascimento"), dataDeNascimento);
+        return (root, query, criteriaBuilder) -> {
+            if (dataDeNascimento == null)
+                return criteriaBuilder.conjunction();
+            return criteriaBuilder.equal(root.get("dataDeNascimento"), dataDeNascimento);
+        };
     }
 }
